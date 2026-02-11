90839a223d01c5067e3dacd3da14b2397c474bfd
SambhabBose <bosesambhab2020@gmail.com>
2026-02-11 16:55:18 +0530

    add variables and datatypes basics

---
+++ 01_basics/variables.py
@@ -0,0 +1,18 @@
+print("Hello Python!")
+
+
+name = "Python"
+age = 22
+is_active = True
+price = 12.55
+
+# print(name)
+# print(age)
+# print(isActive)
+# print(price)
+
+# type checking
+print(type(name)) # string
+print(type(age))    # int
+print(type(isActive))   # boolean
+print(type(price))  #float
