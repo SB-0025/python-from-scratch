4d866891c70ee9a7de33dcb3d329180daebfb105
SambhabBose <bosesambhab2020@gmail.com>
2026-02-16 18:56:29 +0530

    add relational  and logical operators

---
+++ 01_basics/operators_02.py
@@ -0,0 +1,25 @@
+# Relational Operator -> <, >, <=, >=, ==, !=
+
+a = 12
+b = 14
+
+print(a > b) # False
+print(a >= b) # False
+print(a < b)  # True
+print(a <= b) # True
+print(a == b) # False
+print(a != b) # True
+
+
+
+
+
+# Logical Operators -> and, or, not
+
+num1 = 102
+num2 = 46
+
+print(num1>num2 and num1<num2)
+print(num1==num2 or num1>num2)
+print(not(num1==num2 or num1<num2))
+
